2c73407019039bb138543992166a357f0dd8698b
Akshay <akshaykant5006@gmail.com>
2024-01-24 23:09:15 +0530

    search functionality

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,16 @@
 import React from "react";
 
 const Card = (props) => {
-  console.log(props.conValue);
+  // console.log(props.conValue);
 
 
 
   return <div className="card">
-        {/* <img src={props?.conValue?.flags?.png} alt="" width="180px" /> */}
+        <img src={props?.conValue?.flags?.png} alt={props?.conValue?.flags?.alt} width="180px" />
               <h1>{props?.conValue?.name?.common}</h1>
               <h4>Population:{props?.conValue?.population}</h4>
               <h4>Region:{props?.conValue?.region}</h4>
-              {/* <h4>Capital:{props?.conValue?.capital[0]}</h4> */}
+              <h4>Capital:{props?.conValue?.capital}</h4>
   </div>
 };
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,25 +5,54 @@ import Card from "./Card";
 
 const Home = () => {
   const [conData, setConData] = useState([]);
+  const [filterConData, setFilterConData] = useState([]);
 
   const fetchData = async () => {
     const data = await fetch("https://restcountries.com/v3.1/all");
     const jsonData = await data.json();
     setConData(jsonData);
+    setFilterConData(jsonData);
+    console.log(conData)
   };
+
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return conData.length == 0 ? (
+  const searchHandler = (e) => {
+    let inputdata = e.target.value || "";
+    if (inputdata !== "") {
+      let searchData = conData.filter((value) => {
+     
+        return value?.name?.common
+          .toLowerCase()
+          .includes(inputdata.toLowerCase());
+      });
+      setFilterConData(searchData);
+    } else {
+      setFilterConData(conData);
+    }
+  };
+
+  return filterConData.length == 0 ? (
     <h1>Loading..</h1>
   ) : (
-    <div className="home-container">
-      {conData.map((value) => <Card conValue={value} />  )}
+    <div className="home">
+      <div>
+        <input
+          type="text"
+          onChange={searchHandler}
+          placeholder="Search for a country"
+        />
+      </div>
+      <div className="home-container">
+        {filterConData.map((value, index) => (
+          <Card conValue={value} key={index} />
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Home;
-
